a3ecf6af503e961771fc9dcedd68008aced6f13c
oafish mirror <theparzival8@gmail.com>
2025-02-02 13:54:43 -0300

    terceira parte da automatizaçao

--- cypress/e2e/3-site-ebac/login.cy.js
+++ cypress/e2e/3-site-ebac/login.cy.js
@@ -15,16 +15,28 @@ describe('funcionalidade: login', () => {
 
     })
 
-    it('deve falhar no login', () =>{
+    it('deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
         
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('memailteste12@teste1.com')
+        cy.get('#password').type('melhor senha de teste')
+        cy.get('#rememberme').check()
+        cy.get('.woocommerce-form > .button').click()
+
+        ///cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+
+        cy.get('.woocommerce-error').should('exist')
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('emailteste12@teste1.com')
         cy.get('#password').type('mmelhor senha de teste')
         cy.get('#rememberme').check()
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail emailteste12@teste1.com está incorreta. Perdeu a senha?')
 
-    })
+    });
 
 
 })
