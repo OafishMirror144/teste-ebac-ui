66088e0e8a9d80d3e449579ab3d4ea6c3a54351f
oafish mirror <theparzival8@gmail.com>
2025-02-05 17:02:46 -0300

    testes negativos e hooks

--- cypress/e2e/3-site-ebac/login.cy.js
+++ cypress/e2e/3-site-ebac/login.cy.js
@@ -1,10 +1,15 @@
 ///<reference types="cypress"/>
 
 describe('funcionalidade: login', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('deve fazer login com sucesso', () =>{
-
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('emailteste12@teste1.com')
         cy.get('#password').type('melhor senha de teste')
         cy.get('#rememberme').check()
@@ -12,24 +17,17 @@ describe('funcionalidade: login', () => {
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, emailteste12 (não é emailteste12? Sair)')
         cy.get('.woocommerce-MyAccount-navigation-link--customer-logout > a').click()
 
-
     })
 
     it('deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
-        
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('memailteste12@teste1.com')
         cy.get('#password').type('melhor senha de teste')
         cy.get('#rememberme').check()
         cy.get('.woocommerce-form > .button').click()
-
-        ///cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
-
         cy.get('.woocommerce-error').should('exist')
     })
 
     it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('emailteste12@teste1.com')
         cy.get('#password').type('mmelhor senha de teste')
         cy.get('#rememberme').check()
